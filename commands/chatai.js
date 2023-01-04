94f8c01cfaf6d43fb387702ec0e38092a45e1ea4
HaradaYasuo <academic.dev0531@gmail.com>
2023-01-04 13:24:39 -0500

    add trycatch

--- commands/chatai.js
+++ commands/chatai.js
@@ -1,12 +1,17 @@
 module.exports = async (message, openai, prompt) => {
-  const completion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    // prompt: args.join(' '),
-    prompt: prompt,
-    temperature: 1,
-    max_tokens: 2049,
-    // stop: ['ChatGPT:', `${message.author.username}:`],
-  });
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      // prompt: args.join(' '),
+      prompt: prompt,
+      temperature: 1,
+      max_tokens: 2049,
+      // stop: ['ChatGPT:', `${message.author.username}:`],
+    });
 
-  message.reply(completion.data.choices[0].text);
+    message.reply(completion.data.choices[0].text);
+  } catch (error) {
+    console.log(error.response.data.error.message);
+    return handleError(message, error.response.data.error.message);
+  }
 };
