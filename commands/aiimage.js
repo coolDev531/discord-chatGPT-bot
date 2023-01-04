eaeb405c2d26ff8bc5371606f249aea79780bc23
HaradaYasuo <academic.dev0531@gmail.com>
2023-01-03 22:41:52 -0500

    fix

--- commands/aiimage.js
+++ commands/aiimage.js
@@ -1,14 +1,20 @@
 const buildEmbed = require('../utils/buildEmbed');
+const { handleError } = require('../utils/errorHandler');
 
 module.exports = async (message, openai, prompt) => {
-  message.reply('one moment, crafting an image...');
+  try {
+    message.reply('one moment, crafting an image...');
 
-  const imageResp = await openai.createImage({
-    prompt,
-    n: 1,
-    size: '1024x1024',
-  });
-  const imageUrl = imageResp.data.data[0].url;
-  const embed = buildEmbed(imageUrl);
-  return message.reply({ embeds: [embed] });
+    const imageResp = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+    });
+    const imageUrl = imageResp.data.data[0].url;
+    const embed = buildEmbed(imageUrl);
+    return message.reply({ embeds: [embed] });
+  } catch (error) {
+    console.log(error.response.data.error.message);
+    return handleError(message, error.response.data.error.message);
+  }
 };
--- commands/aiimagevariation.js
+++ commands/aiimagevariation.js
@@ -4,6 +4,7 @@ const sharp = require('sharp');
 const fsPromise = require('fs/promises');
 const buildEmbed = require('../utils/buildEmbed');
 const path = require('path');
+const { handleError } = require('../utils/errorHandler');
 
 module.exports = async (message, openai) => {
   message.reply('one moment, crafting an image...');
@@ -16,18 +17,25 @@ module.exports = async (message, openai) => {
   })
     .then(function (response) {
       const fileName = `${toBeConverted.id + Date.now()}`;
-      response.data.pipe(fs.createWriteStream(`./files/${fileName}.png`));
+      response.data.pipe(
+        fs.createWriteStream(
+          path.resolve(__dirname, `../files/${fileName}.png`)
+        )
+      );
       return response.data.on('end', async () => {
-        const originalImage = path.resolve(__dirname, `files/${fileName}.png`);
+        const originalImage = path.resolve(
+          __dirname,
+          `../files/${fileName}.png`
+        );
         await sharp(originalImage)
           .resize({
             width: 1024,
             height: 1024,
           })
-          .toFile(path.resolve(__dirname, `files/${fileName}-resized.png`));
+          .toFile(path.resolve(__dirname, `../files/${fileName}-resized.png`));
 
         const buffer = await fsPromise.readFile(
-          `./files/${fileName}-resized.png`
+          path.resolve(__dirname, `../files/${fileName}-resized.png`)
         );
 
         buffer.name = toBeConverted.name || 'image'; // set the name of the buffer to the name of the file
@@ -40,8 +48,12 @@ module.exports = async (message, openai) => {
         const imageUrl = imageResp.data.data[0].url;
         const embed = buildEmbed(imageUrl);
         message.reply({ embeds: [embed] });
-        await fsPromise.unlink(`./files/${fileName}.png`); // delete the file when done
-        await fsPromise.unlink(`./files/${fileName}-resized.png`); // delete the file when done
+        await fsPromise.unlink(
+          path.resolve(__dirname, `../files/${fileName}.png`)
+        ); // delete the file when done
+        await fsPromise.unlink(
+          path.resolve(__dirname, `../files/${fileName}-resized.png`)
+        ); // delete the file when done
         return;
       });
     })
