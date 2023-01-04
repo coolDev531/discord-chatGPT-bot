194a0783d5f8db0c053bc1f086a9ce62f3fc47fe
HaradaYasuo <academic.dev0531@gmail.com>
2023-01-04 13:27:52 -0500

    refactor

--- commands/aiimagevariation.js
+++ commands/aiimagevariation.js
@@ -7,58 +7,55 @@ const path = require('path');
 const { handleError } = require('../utils/errorHandler');
 
 module.exports = async (message, openai) => {
-  message.reply('one moment, crafting an image...');
-  const toBeConverted = [...message.attachments.values()][0];
+  try {
+    message.reply('one moment, crafting an image...');
+    const toBeConverted = [...message.attachments.values()][0];
 
-  return axios({
-    method: 'get',
-    url: toBeConverted.url,
-    responseType: 'stream',
-  })
-    .then(function (response) {
-      const fileName = `${toBeConverted.id + Date.now()}`;
-      response.data.pipe(
-        fs.createWriteStream(
-          path.resolve(__dirname, `../files/${fileName}.png`)
-        )
-      );
-      return response.data.on('end', async () => {
-        const originalImage = path.resolve(
-          __dirname,
-          `../files/${fileName}.png`
-        );
-        await sharp(originalImage)
-          .resize({
-            width: 1024,
-            height: 1024,
-          })
-          .toFile(path.resolve(__dirname, `../files/${fileName}-resized.png`));
-
-        const buffer = await fsPromise.readFile(
-          path.resolve(__dirname, `../files/${fileName}-resized.png`)
-        );
-
-        buffer.name = toBeConverted.name || 'image'; // set the name of the buffer to the name of the file
-
-        const imageResp = await openai.createImageVariation(
-          buffer,
-          1,
-          '1024x1024'
-        );
-        const imageUrl = imageResp.data.data[0].url;
-        const embed = buildEmbed(imageUrl);
-        message.reply({ embeds: [embed] });
-        await fsPromise.unlink(
-          path.resolve(__dirname, `../files/${fileName}.png`)
-        ); // delete the file when done
-        await fsPromise.unlink(
-          path.resolve(__dirname, `../files/${fileName}-resized.png`)
-        ); // delete the file when done
-        return;
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      return handleError(message, 'error');
+    const response = await axios({
+      method: 'get',
+      url: toBeConverted.url,
+      responseType: 'stream',
     });
+
+    const fileName = `${toBeConverted.id + Date.now()}`;
+
+    response.data.pipe(
+      fs.createWriteStream(path.resolve(__dirname, `../files/${fileName}.png`))
+    );
+
+    return response.data.on('end', async () => {
+      const originalImage = path.resolve(__dirname, `../files/${fileName}.png`);
+      await sharp(originalImage)
+        .resize({
+          width: 1024,
+          height: 1024,
+        })
+        .toFile(path.resolve(__dirname, `../files/${fileName}-resized.png`));
+
+      const buffer = await fsPromise.readFile(
+        path.resolve(__dirname, `../files/${fileName}-resized.png`)
+      );
+
+      buffer.name = toBeConverted.name || 'image'; // set the name of the buffer to the name of the file
+
+      const imageResp = await openai.createImageVariation(
+        buffer,
+        1,
+        '1024x1024'
+      );
+      const imageUrl = imageResp.data.data[0].url;
+      const embed = buildEmbed(imageUrl);
+      message.reply({ embeds: [embed] });
+      await fsPromise.unlink(
+        path.resolve(__dirname, `../files/${fileName}.png`)
+      ); // delete the file when done
+      await fsPromise.unlink(
+        path.resolve(__dirname, `../files/${fileName}-resized.png`)
+      ); // delete the file when done
+      return;
+    });
+  } catch (error) {
+    console.log(error.response.data.error.message);
+    return handleError(message, error.response.data.error.message);
+  }
 };
