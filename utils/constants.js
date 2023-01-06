5da15b0adc2302c595f743775f4946112f91570e
HaradaYasuo <academic.dev0531@gmail.com>
2023-01-06 15:28:25 -0500

    fix

---
+++ utils/constants.js
@@ -0,0 +1,28 @@
+const COMMAND_ALIASES = {
+  chatai: ['chatai', 'chat', 'chatgpt', 'chatbot', 'aichat'],
+  aiimage: ['aiimage', 'imageai', 'image', 'makeimage'],
+  aiimagevariation: [
+    'aiimagevariation',
+    'aiimagevar',
+    'imagevariation',
+    'imagevar',
+    'reviseimage',
+    'aireviseimage',
+  ],
+  aiimageedit: ['imageedit', 'editimage', 'aiimageedit', 'aieditimage'],
+  randomimage: [
+    'randomimage',
+    'random',
+    'randomimg',
+    'randompic',
+    'airandomimage',
+    'airandompic',
+    'airandomimg',
+    'rand',
+  ],
+  codeai: ['codeai', 'code', 'codegpt', 'codebot', 'aicode'],
+};
+
+module.exports = {
+  COMMAND_ALIASES,
+};
